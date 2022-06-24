6d0c88a69decb8315d7fd5bc4758db70f4de51a7
Любовь Воронова <darkluba@yandex.ru>
2022-06-24 15:35:31 +0300

    Первая отправка на ревью

--- yatube/posts/forms.py
+++ yatube/posts/forms.py
@@ -1,9 +1,10 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
+
 from .models import Post
 
+
 class PostForm(forms.ModelForm):
-    
     class Meta(UserCreationForm.Meta):
         model = Post
         fields = ['text', 'group']
--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -25,32 +25,38 @@ urlpatterns = [
     ),
     path(
         'password_change/done/',
-        PasswordChangeDoneView.as_view(template_name='users/password_change_done.html'),
+        PasswordChangeDoneView
+        .as_view(template_name='users/password_change_done.html'),
         name='password_change_done'
-        ),
+    ),
     path(
         'password_change/',
-        PasswordChangeView.as_view(template_name='users/password_change_form.html'),
+        PasswordChangeView.
+        as_view(template_name='users/password_change_form.html'),
         name='password_change'
-        ),
+    ),
     path(
         'reset/done/',
-        PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'),
+        PasswordResetCompleteView.
+        as_view(template_name='users/password_reset_complete.html'),
         name='reset_complete'
-        ),
+    ),
     path(
         'reset/<uidb64>/<token>/',
-        PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'),
+        PasswordResetConfirmView.
+        as_view(template_name='users/password_reset_confirm.html'),
         name='password_reset_confirm'
-        ),
+    ),
     path(
         'password_reset/done/',
-        PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),
+        PasswordResetDoneView.
+        as_view(template_name='users/password_reset_done.html'),
         name='password_reset_done'
-        ),
+    ),
     path(
         'password_reset_form/',
-        PasswordResetView.as_view(template_name='users/password_reset_form.html'),
+        PasswordResetView.
+        as_view(template_name='users/password_reset_form.html'),
         name='password_reset_form'
-        ),
+    ),
 ]
