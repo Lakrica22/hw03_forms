c4d996ccd1e9b103bba12c02dd93075857ead437
Любовь Воронова <darkluba@yandex.ru>
2022-06-26 10:20:38 +0300

    Вторая отправка на ревью.Замена

--- yatube/posts/utils.py
+++ yatube/posts/utils.py
@@ -1,8 +1,10 @@
 from django.core.paginator import Paginator
 
+POSTS_ON_LIST: int = 10
+
 
 def get_page_context(queryset, request):
-    paginator = Paginator(queryset, 10)
+    paginator = Paginator(queryset, POSTS_ON_LIST)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     return {
